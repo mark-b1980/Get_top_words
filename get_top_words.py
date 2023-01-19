8bf15e14a825c36f8467870699ac3fa12055e31b
mark-b1980 <mark.b@post.cz>
2023-01-19 12:11:42 +0100

    Initial commit v0.1

---
+++ get_top_words.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import sys
+
+# Check if programm started with all arguments
+if len(sys.argv) < 3 or "-h" in sys.argv:
+    print("USAGE: get_top_words.py INPUTFILE MIN-LENGTH [TOP-X]\n")
+    quit()
+
+# Check if 2nd argument is an integer
+try:
+    sys.argv[2] = int(sys.argv[2])
+    if len(sys.argv) == 3:
+        sys.argv.append(20)
+    else:
+        sys.argv[3] = int(sys.argv[3])
+except ValueError:
+    print("MIN-LENGTH and TOP-X must be a integer!")
+    quit()
+
+# Read file and parse data
+words = {}
+try:
+    with open(sys.argv[1], "r") as f:
+        for line in f:
+            line = line.lower().replace("\n", " ")
+            line = line.replace(".", " ").replace("!", " ").replace("?", " ")
+            line = line.replace(",", " ").replace(";", " ").replace("-", " ")
+            line = line.replace("@", " ").replace("#", " ").replace(":", " ")
+
+            for word in line.split(" "):
+                if len(word) >= sys.argv[2]:
+                    if not words.get(word, False):
+                        words[word] = 1
+                    else:
+                        words[word] += 1
+except:
+    print("Can't read INPUTFILE!")
+    quit()
+
+# Output words with top 20 count 
+for i in sorted(words.values(), reverse=True)[0:sys.argv[3]]:
+    print(f"{i:>4}: ", end="")
+    for word, count in words.items():
+        if count == i:
+            print(f"{word} ", end="")
+    print()
+
